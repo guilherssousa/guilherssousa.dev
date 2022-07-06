d520bac7d0369c43c3a7b034516430c9eae645d3
Guilherme Sousa <guilherssousa@gmail.com>
2022-07-06 20:56:08 -0300

    change image resolution

--- components/work-card.tsx
+++ components/work-card.tsx
@@ -16,8 +16,8 @@ const WorkCard: React.FC<Props> = ({ name, imageUrl, description, link }) => {
           <Image
             src={imageUrl}
             alt={name}
-            width={480}
-            height={270}
+            width={360}
+            height={240}
             placeholder="blur"
             loading="lazy"
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8f5KhHgAG4AJJ/M2wwwAAAABJRU5ErkJggg=="
