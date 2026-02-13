9b495f5f867d362a0aa2939d760280f94229e174
Guilherme Sousa <guilherssousa@gmail.com>
2026-02-12 22:32:38 -0300

    Add RSS feed using Zenblog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,21 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
+import { zenblog } from '@/lib/blog';
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
+
+	const posts = await zenblog.posts.list();
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
+		items: posts.data.map((post) => ({
+			title: post.title,
+			description: post.excerpt,
 			link: `/blog/${post.slug}/`,
+			...post,
 		})),
 	});
 }
